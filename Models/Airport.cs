ad501e6b77e444effa3a80f02b180bc2e2e673a9
alejandropj <alejandroparrajimenez@gmail.com>
2024-04-04 12:40:44 +0200

    Add project files.

---
+++ Models/Airport.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AeropuertosAPJ.Models
+{
+    public class Airport
+    {
+        [JsonProperty("@odata.id")]
+        public string ODataId { get; set; }
+
+        [JsonProperty("@odata.editLink")]
+        public string ODataEditLink { get; set; }
+
+        [JsonProperty("IcaoCode")]
+        public string IcaoCode { get; set; }
+
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("IataCode")]
+        public string IataCode { get; set; }
+    }
+}
---
+++ Services/ServiceAeropuertos.cs
@@ -0,0 +1,25 @@
+﻿using AeropuertosAPJ.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AeropuertosAPJ.Services
+{
+    public class ServiceAeropuertos
+    {
+        public async Task<AirportList> GetAirportsListAsync()
+        {
+            WebClient client = new WebClient();
+            client.Headers["content-type"] = "application/json";
+            string url = "https://services.odata.org/V4/(S(2esholowikwyef30ogqjzbvf))/TripPinServiceRW/Airports";
+            string dataJson = await client.DownloadStringTaskAsync(url);
+            AirportList airports = JsonConvert.DeserializeObject<AirportList>(dataJson);
+            return airports;
+        }
+        
+    }
+}
